134586c623039536bf98b142e819d50c3428b2a7
mistertandon <enggparveshtandon@gmail.com>
2022-11-05 20:45:07 +0530

    feat: add support for scss|sass|css file

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s?css$/i,
+        test: /\.(s[ac]|c)ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader",
