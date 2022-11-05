b31598895bdf33e0df8b3fabc3a112e13763e52f
mistertandon <enggparveshtandon@gmail.com>
2022-11-05 17:09:35 +0530

    feat: add global variable configuration to application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const elevenShieldRecipe = {
-  leatherStrings: 2,
-  ironIngot: 1,
-  refinedMoonstoned: 4,
+  leatherStrings: 1,
+  ironIngot: 2,
+  refinedMoonstoned: 3,
+};
+
+const elevenGauntletsRecipe = {
+  ...elevenShieldRecipe,
+  leather: 4,
+  refinedMoonstoned: 5,
 };
 
 console.log("elevenShieldRecipe: ", elevenShieldRecipe);
+console.log("elevenGauntletsRecipe: ", elevenGauntletsRecipe);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
-const path = require('path');
-module.exports = {
-    mode: "development",
-    devServer:{
-        static:{
-            directory: path.join(__dirname, 'dist')
-        }
-    }
+const path = require("path");
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
+
+module.exports = {
+  mode,
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
