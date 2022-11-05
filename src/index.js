541444d11cb87bec94084d6a60f7535f35712b77
mistertandon <enggparveshtandon@gmail.com>
2022-11-05 15:35:20 +0530

    feat: configure npm run start command and up the application

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const elevenShieldRecipe = {
+  leatherStrings: 2,
+  ironIngot: 1,
+  refinedMoonstoned: 4,
+};
+
+console.log("elevenShieldRecipe: ", elevenShieldRecipe);
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    mode: "development",
+    devServer:{
+        static:{
+            directory: path.join(__dirname, 'dist')
+        }
+    }
+}
